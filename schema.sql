39808c609213773e3a469b96ec5739e89c20ad80
Alec Brunelle <alec@alec.coffee>
2017-07-03 16:57:21 -0400

    lel

--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE sb_user(
   username VARCHAR(50) NOT NULL,
   steamid VARCHAR(50) NOT NULL,
   steamvanity VARCHAR(50),
-  slackid VARCHAR(50)
+  slackid VARCHAR(50),
+  integration_fk VARCHAR(50) NOT NULL
 );
